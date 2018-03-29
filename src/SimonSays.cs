c70a076758d7d9f6fe692774856c94a654d97f25
aattebe1 <37160977+aattebe1@users.noreply.github.com>
2018-03-29 17:45:51 -0400

    Add files via upload

--- src/SimonSays.cs
+++ src/SimonSays.cs
@@ -16,6 +16,17 @@ using WiringPi;
 
 public class SimonSays
 {
+	/* Private Constants */
+	private const int SWITCH_1 = 0x0D; // PIN33
+	private const int SWITCH_2 = 0x13; // PIN35
+	private const int SWITCH_3 = 0x10; // PIN36
+	private const int LED_1 = 0x1A;    // PIN37
+	private const int LED_2 = 0x14;    // PIN38
+	private const int LED_3 = 0x15;    // PIN40
+	private const int OFF = 0x01;      // 3.3V
+	private const int ON = 0x00;       // 0V
+	
+	
 	//-----------------------------------------------------------------------------------
 	//  Main method - The entry point for the program
 	//       params:  String array
@@ -23,29 +34,65 @@ public class SimonSays
 	//-----------------------------------------------------------------------------------
 	public static void Main(String[] args)
 	{
+		/* Save the number of the last switch pressed */
+		byte switchPressed = 0;
+		
+		/* Variable declaration section */
+		bool correctInput = false;
+		
 		/* Create an LEDGame object */
-        LEDGame newGame = new LEDGame(10, 1);
-
-        /* Set cheatmode status */
-        if (args[0].ToLowerInvariant() == "cheatmode")
-        {
-            newGame.CheatMode(true);
-        }
-        else
-        {
-            newGame.CheatMode(false);
-        }
-
-        for (int i = 0; i < newGame.Length(); i++)
-        {
-            /* Display the LED pattern */
-            newGame.LEDPattern();
-
-            /* Check input, exit if incorrect */
-            if (!newGame.CheckInput())
-            {
-                break;
-            }
-        }
+		LEDGame newGame = new LEDGame(10, 1,
+			new int[]
+			{
+				SWITCH_1,
+				SWITCH_2,
+				SWITCH_3,
+				LED_1,
+				LED_2,
+				LED_3,
+				OFF,
+				ON
+			});
+		
+		/* Set cheatmode status */
+		try
+		{
+			if (args[0].ToLowerInvariant() == "cheatmode")
+			{
+				newGame.CheatMode(true);
+			}
+		}
+		catch (IndexOutOfRangeException)
+		{
+			newGame.CheatMode(false);
+		}
+		
+		for (int i = 0; i < newGame.Length(); i++)
+		{
+			/* Reset input checking variable */
+			correctInput = false;
+			
+			/* Display the LED pattern */
+			newGame.LEDPattern(i);
+			
+			/* Check input */
+			newGame.CheckInput(i, ref correctInput, ref switchPressed);
+			
+			//while(!correctInput)
+			//{
+				/* Reset the number of the pressed switch */
+				//switchPressed = 0;
+				
+				/* Wait for switch push */
+				/*while (switchPressed == 0)
+				{
+					if (WiringPi.GPIO.digitalRead(SWITCH_1) == ON)
+					{
+						switchPressed = 1; // Switch 1 pressed
+						correctInput = true;
+					}
+				}*/
+			//}
+		}
 	}
 }
